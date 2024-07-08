c3b2b22b6c99060a4398059543e543b9d52c433f
shahanursiam <shahanurislamsiam24@gmail.com>
2024-07-09 00:30:49 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <div className=' flex justify-center items-center h-screen flex-col'>
         <h1 className=' text-2xl'>InshaAllah, I will come back with my best version</h1>
         <p>This is <span className=' text-xl'>shahanur islam siam</span></p>
+        <p>InshaAllah I will</p>
       </div>
     </>
   );
