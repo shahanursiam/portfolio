c2e89d583843d83cf2ad5f0d630e53336a70c455
shahanursiam <shahanurislamsiam24@gmail.com>
2024-07-09 00:12:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div className=' flex justify-center items-center h-screen flex-col'>
+        <h1 className=' text-2xl'>InshaAllah, I will come back with my best version</h1>
+        <p>This is <span className=' text-xl'>shahanur islam siam</span></p>
+      </div>
+    </>
+  );
+};
+
+export default App;
