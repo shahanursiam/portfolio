654cf8cd3bbdb69a111969f2a1fa9953b548e7eb
shahanursiam <shahanurislamsiam24@gmail.com>
2024-07-09 18:08:48 +0600

    delete file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      <div className=' flex justify-center items-center h-screen flex-col text-center gap-4'>
+      <div className=' flex justify-center items-center h-screen flex-col text-center gap-4 '>
         <h1 className=' text-2xl'>InshaAllah, I will come back with my best version</h1>
         <p>This is <span className=' text-xl'>shahanur islam siam</span></p>
         <p>InshaAllah I will</p>
